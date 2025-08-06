257aeacb82b3336b7633b7bef98f491d022cc0dd
MrShanks <simonestaffoli@gmail.com>
2025-08-06 09:57:43 +0200

    feat: restructure code + init server

--- main.go
+++ main.go
@@ -2,60 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 )
-
-type User struct {
-	Firstname string
-	Lastname  string
-	Email     string
-	Debt      float64
-	Credit    float64
-}
-
-type Metadata struct {
-	CreatedDate  time.Time
-	LastModified time.Time
-}
-
-type Expense struct {
-	Amount    float64
-	Currency  string
-	Payer     User
-	Recurrent bool
-	Metadata
-}
-
-func (u *User) Add(amount float64) {
-	u.Credit += amount
-}
-
-func (g *Group) AddExpense(amount float64, currency string, user *User, recurrent bool) *Expense {
-	metadata := Metadata{
-		CreatedDate:  time.Now(),
-		LastModified: time.Now(),
-	}
-	expense := &Expense{
-		Amount:    amount,
-		Currency:  currency,
-		Payer:     *user,
-		Recurrent: recurrent,
-		Metadata:  metadata,
-	}
-
-	for _, m := range g.Members {
-		if m.Firstname == user.Firstname {
-			m.Add(amount)
-		}
-		m.Add(-amount / float64(len(g.Members)))
-	}
-
-	return expense
-}
-
-type Group struct {
-	Members []*User
-}
 
 func main() {
 
@@ -79,4 +29,17 @@ func main() {
 	for _, m := range netflix.Members {
 		fmt.Println(m.Firstname, m.Credit, m.Debt)
 	}
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	s := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
 }
